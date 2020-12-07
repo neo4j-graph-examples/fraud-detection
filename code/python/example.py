f89c4856b7b2a7d6552dfbf18a203b1be6e3e9ce
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2020-12-07 12:12:56 +0000

    Updating sandbox fraud-detection

---
+++ code/python/example.py
@@ -0,0 +1,23 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "bolt://<HOST>:<BOLTPORT>", 
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (m:Merchant{name:$name})<-[:TO]-(:Transaction)<-[:PERFORMED]-(c:Client)
+RETURN c.name as client
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+      name="MYrsa").data())
+
+  for record in results:
+    print(record['client'])
+
+driver.close()
