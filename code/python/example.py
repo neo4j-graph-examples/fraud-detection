a3cfe9bcbf4e1c5f6643ce46cf702021248d102b
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2023-12-11 09:18:21 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/9452d62941fd3de583c79d51df836ec60d1783ac

--- code/python/example.py
+++ code/python/example.py
@@ -1,22 +1,20 @@
-# pip3 install neo4j-driver
+# pip3 install neo4j
 # python3 example.py
 
 from neo4j import GraphDatabase, basic_auth
-
-driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>",
-  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
 MATCH (m:Merchant{name:$name})<-[:TO]-(:Transaction)<-[:PERFORMED]-(c:Client)
 RETURN c.name as client
 '''
 
-with driver.session(database="neo4j") as session:
-  results = session.read_transaction(
-    lambda tx: tx.run(cypher_query,
-                      name="MYrsa").data())
-  for record in results:
-    print(record['client'])
-
-driver.close()
+with GraphDatabase.driver(
+    "neo4j://<HOST>:<BOLTPORT>",
+    auth=("<USERNAME>", "<PASSWORD>")
+) as driver:
+    result = driver.execute_query(
+        cypher_query,
+        name="MYrsa",
+        database_="neo4j")
+    for record in result.records:
+        print(record['client'])
